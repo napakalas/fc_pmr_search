196657389899140b4026f794077344314683bda2
napakalas <yuda.munarko@gmail.com>
2024-03-26 18:23:07 +1300

    tiidy up tool to generate sckan2pmr index

--- tools/create_sckan2pmr.py
+++ tools/create_sckan2pmr.py
@@ -5,15 +5,12 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(description="Creating search index for SCKAN and PMR")
-    parser.add_argument('--annotation', dest='annotations', help='Path FC annotation json file', default=None)
+    parser.add_argument('--min-sim', dest='minimalSimilarity', help='Minimal similarity.', default=0.7)
     args = parser.parse_args()
 
     searcher = PMRSearcher()
-    searcher.generate_term_to_pmr_save(min_sim=0.7)
-    searcher.check_and_generate_annotation_completeness(args.annotations)
+    searcher.generate_term_to_pmr_save(min_sim=args.min_sim)
 
-    
-    
 #===============================================================================
 
 if __name__ == '__main__':
@@ -22,5 +19,5 @@ if __name__ == '__main__':
 #===============================================================================
 
 # Running script:
-# python create_sckan2pmr.py --annotation directory_to_store
+# python create_sckan2pmr.py --min-sim 'Minimal similarity'
     
