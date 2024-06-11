40fc865bd36c9eb4ce3e33ac823c31c0c0353ee0
napakalas <yuda.munarko@gmail.com>
2024-06-11 19:17:39 +1200

    creating file to zip file for consistencies

---
+++ tools/zip_sckan2pmr.py
@@ -0,0 +1,14 @@
+import json
+import zipfile
+from pmrsearch.setup import METADATA_FILE, SCKAN2PMR, CURRENT_PATH
+
+with open(METADATA_FILE, 'r') as f:
+    metadata = json.load(f)
+
+release_name = f'{CURRENT_PATH}/output/release-sckan2pmr-{metadata["pmrindexer_version"]}.zip'
+
+compression = zipfile.ZIP_DEFLATED
+zf = zipfile.ZipFile(release_name, mode="w")
+zf.write(METADATA_FILE, METADATA_FILE.split('/')[-1], compression)
+zf.write(SCKAN2PMR, SCKAN2PMR.split('/')[-1], compression)
+zf.close()
