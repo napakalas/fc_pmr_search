d5eb1d305efbf79f42db592d582a848796e34b4b
napakalas <yuda.munarko@gmail.com>
2024-06-11 13:38:10 +1200

    add optional for cleanExtraction

--- tools/build_index.py
+++ tools/build_index.py
@@ -6,11 +6,12 @@ def main():
 
     parser = argparse.ArgumentParser(description="Creating search index for SCKAN and PMR")
     parser.add_argument('--workspace', dest='workspaceDir', help='Path to PMR workspaces')
+    parser.add_argument('--clean-extraction', dest='cleanExtraction', help='Clean extraction will crawl all knowledges', action='store_true')
     args = parser.parse_args()
 
     pmr_indexer = PMRIndexer(pmr_workspace_dir=args.workspaceDir)
 
-    pmr_indexer.create_search_index()
+    pmr_indexer.create_search_index(clean_extraction=args.cleanExtraction)
     
     
 #===============================================================================
@@ -20,4 +21,7 @@ if __name__ == '__main__':
 
 #===============================================================================
 
-# poetry run python tools/build_index.py --workspace /Users/ymun794/Documents/Git_PhD/casbert-indexer/casbert_indexer/workspaces
+# poetry run python tools/build_index.py
+# poetry run python tools/build_index.py --clean-extraction
+# poetry run python tools/build_index.py --workspace ../pmrsearch/workspaces
+# poetry run python tools/build_index.py --workspace ../pmrsearch/workspaces --clean-extraction
