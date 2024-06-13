6ddb76b8236a68e58510e7ebfd43f772af923c99
napakalas <yuda.munarko@gmail.com>
2024-06-14 10:21:36 +1200

    exposing PMRSearcher only

--- pmrsearch/__init__.py
+++ pmrsearch/__init__.py
@@ -1,8 +1,6 @@
 #===============================================================================
 
 from pmrsearch.searcher import PMRSearcher
-from pmrsearch.indexer.clusterer import CellmlClusterer
-from pmrsearch.indexer.pmr_crawler import *
 
 #===============================================================================
 
