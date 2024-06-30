53f30411988d3524efc2e7464b4daee07109d0cb
napakalas <yuda.munarko@gmail.com>
2024-07-01 10:59:58 +1200

    update test script

--- tools/test.py
+++ tools/test.py
@@ -1,3 +1,6 @@
+#===============================================================================
+
+from pprint import pprint
 from pmrsearch import PMRSearcher
 
 #===============================================================================
@@ -7,13 +10,13 @@ def main():
     model = PMRSearcher()
     
     print(f'The results of search_all method for query {query}')
-    print(model.search_all(query))
+    pprint(model.search_all(query))
     print(f'The results of search_exposure method for query {query}')
-    print(model.search_exposure(query))
+    pprint(model.search_exposure(query))
     print(f'The results of search_workspacve method for query {query}')
-    print(model.search_workspace(query))
+    pprint(model.search_workspace(query))
     print(f'The results of search_by_cellml method for query {query}')
-    print(model.search_by_cellml(query))
+    pprint(model.search_by_cellml(query))
 
 if __name__ == '__main__':
     main()
