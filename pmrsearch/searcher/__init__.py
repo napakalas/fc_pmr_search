4f891d636554b241f0b59a0b7a3620558542eb55
napakalas <yuda.munarko@gmail.com>
2024-07-01 10:59:43 +1200

    no 'data' field in workspaces

--- pmrsearch/searcher/__init__.py
+++ pmrsearch/searcher/__init__.py
@@ -246,7 +246,7 @@ class PMRSearcher:
 
     def extract_exposure_data(self):
         ## load workspaces
-        workspaces = loadJson(RS_WORKSPACE)['data']
+        workspaces = loadJson(RS_WORKSPACE)
 
         ## functions for authors and description update
         valid_email_regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
