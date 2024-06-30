3ba8cfb50f8d02e42d094406ade941b609a30ea7
napakalas <yuda.munarko@gmail.com>
2024-07-01 10:58:53 +1200

    indexing update

--- pmrsearch/indexer/__init__.py
+++ pmrsearch/indexer/__init__.py
@@ -1,5 +1,4 @@
 import os
-import select
 import pandas as pd
 import re
 import rdflib
@@ -140,7 +139,7 @@ class PMRIndexer:
         term_ids = []
         all_documentation = []
         file_types = ['cellml', 'rdf', 'html', 'md', 'rst']
-        workspaces = loadJson(RS_WORKSPACE)['data']
+        workspaces = loadJson(RS_WORKSPACE)
         
         cellml_to_terms = {}
 
@@ -152,13 +151,14 @@ class PMRIndexer:
             cellmls = search_data['cellml']
             selected_workspaces = {}
             for workspace_url, workspace in workspaces.items():
-                if not workspace['hasExtracted']:
-                    selected_workspaces[workspace_url] = workspace
-                    for cellml_url in workspace.get('cellml'):
+                if not workspace.get('hasExtracted'):
+                    for cellml_url in set(workspace.get('cellml', [])):
                         del cellmls[cellml_url]
+                    workspace['cellml'] = []
+                    selected_workspaces[workspace_url] = workspace
 
-
-        # sckan all workspaces
+        new_cellmls = []
+        # sckan all selected workspaces
         for workspace_url, workspace in selected_workspaces.items():
             workspace_dir = os.path.join(self.__pmr_workspace_dir, workspace['workingDir'])
             all_files = [file for file in getAllFilesInDir(workspace_dir)
@@ -168,47 +168,50 @@ class PMRIndexer:
             for file_name in all_files:
                 if (file_type:=file_name[file_name.rfind('.') + 1:].lower()) == 'cellml': ## if cellml file found
                     cellml_url = f'{workspace_url}/rawfile/{workspace["commit"]}/{file_name[len(workspace_dir) + 1:]}'
-                    if cellml_url not in cellmls:
-                        cellmls[cellml_url] = {'workspace': workspace_url, 'workingDir': workspace['workingDir'], 'rdfLeaves': []}
-                        if cellml_url not in workspace: workspace['cellml'] = []
-                        workspace['cellml'] += [cellml_url]
-                        # extract from cellml xml
-                        parser = etree.XMLParser(recover=True, remove_comments=True)
-                        root = etree.parse(file_name, parser).getroot()
-                        g = rdflib.Graph()
-                        for rdfElement in root.xpath(".//*[local-name()='RDF']"):
-                            try:
-                                g.parse(data=etree.tostring(rdfElement), format="application/rdf+xml")
-                            except:
-                                log.error(f'Cannot parse RDF in {cellml_url}')
-                        cellmls[cellml_url]['rdfLeaves'] += [url_to_curie(str(o)) for o in g.objects() if url_to_curie(str(o)) is not None]
-                        term_ids += cellmls[cellml_url]['rdfLeaves']
-                        # add exposure information
-                        if 'exposures' in workspace:
-                            cellmls[cellml_url]['exposure'] = list(workspace['exposures'].keys())[-1]
-                        # add sha information
-                        cellmls[cellml_url]['sha'] = workspace['commit']
-                        # add relative cellml path 
-                        cellmls[cellml_url]['cellml'] = file_name[len(workspace_dir) + 1:]
-                        # add cellml_url to its workspace
-                        if 'cellml' not in workspace:
-                            workspace['cellml'] = []
-                        workspace['cellml'] += [cellml_url]
-                        # update documentation
-                        cellmls[cellml_url]['documentation'] = self.__get_cellml_documentation(file_name)
-                        all_documentation += [cellmls[cellml_url]['documentation']]
+                    cellmls[cellml_url] = {'workspace': workspace_url, 'workingDir': workspace['workingDir'], 'rdfLeaves': []}
+                    # extract from cellml xml
+                    parser = etree.XMLParser(recover=True, remove_comments=True)
+                    root = etree.parse(file_name, parser).getroot()
+                    g = rdflib.Graph()
+                    for rdfElement in root.xpath(".//*[local-name()='RDF']"):
+                        try:
+                            g.parse(data=etree.tostring(rdfElement), format="application/rdf+xml")
+                        except:
+                            log.error(f'Cannot parse RDF in {cellml_url}')
+                    cellmls[cellml_url]['rdfLeaves'] += [url_to_curie(str(o)) for o in g.objects() if url_to_curie(str(o)) is not None]
+                    print('new', cellml_url, cellmls[cellml_url]['rdfLeaves'])
+                    term_ids += cellmls[cellml_url]['rdfLeaves']
+                    # add exposure information
+                    if 'exposures' in workspace:
+                        cellmls[cellml_url]['exposure'] = list(workspace['exposures'].keys())[-1]
+                    # add sha information
+                    cellmls[cellml_url]['sha'] = workspace['commit']
+                    # add relative cellml path
+                    cellmls[cellml_url]['cellml'] = file_name[len(workspace_dir) + 1:]
+                    # add cellml_url to its workspace
+                    if 'cellml' not in workspace:
+                        workspace['cellml'] = []
+                    workspace['cellml'] += [cellml_url]
+                    # update documentation
+                    if len(documents:=self.__get_cellml_documentation(file_name)) > 0:
+                        cellmls[cellml_url]['documentation'] = documents
+                        all_documentation += documents
+                    # add to new_cellml to be checked later
+                    new_cellmls += [cellml_url]
                 elif file_type == 'rdf': ## if rdf file found, add to cellml files
                     try:
                         g_rdf.parse(file_name)
                     except:
                         log.error(f'Cannot parse RDF in {file_name}')
                 elif file_type in ['html', 'md', 'rst']:
-                    workspace_documentation += self.__get_cellml_documentation(file_name)
+                    if len(documents:=self.__get_cellml_documentation(file_name)) > 0:
+                        workspace_documentation += documents
             
             for cellml_url in workspace.get('cellml', []):
                 cellmls[cellml_url]['rdfLeaves'] = list(set(cellmls[cellml_url]['rdfLeaves'] + list(g_rdf.objects())))
                 # extract documentation
-                cellmls[cellml_url]['documentation'] = list(set(cellmls[cellml_url]['documentation']+workspace_documentation))
+                if len(cellml_doc:=cellmls[cellml_url].get('documentation', [])) > 0 or len(workspace_documentation) > 0:
+                    cellmls[cellml_url]['documentation'] = list(set(cellml_doc + workspace_documentation))
                 ## load cellml_to_terms
                 cellml_to_terms[cellml_url] = cellmls[cellml_url]['rdfLeaves']
 
@@ -307,47 +310,83 @@ class PMRIndexer:
         ent_embeddings = self.__biobert_model.encode(all_ents, convert_to_tensor=True)
         
         # exploring cellml files for any documentation
-        for cellml_id, cellml in tqdm(cellmls.items()):
-            if len(cellml_ents:=[ent for doc in cellml['documentation'] for ent in doc_ents[doc]]) == 0:
-                cellml_emb = torch.mean(torch.stack([doc_embeddings[all_documentation.index(doc)] for doc in cellml['documentation']]), dim=0)
+        for cellml_url in tqdm(new_cellmls):
+            cellml_id = cellmls[cellml_url]
+            if len(cellml_ents:=[ent for doc in cellml_id.get('documentation',[]) for ent in doc_ents[doc]]) == 0:
+                if len(cellml_id.get('documentation',[])) > 0:
+                    # condition where there is documentation but no entity detected
+                    cellml_emb = torch.mean(torch.stack([doc_embeddings[all_documentation.index(doc)] for doc in cellml_id['documentation']]), dim=0)
+                elif len(rdf_leaves:=cellml_id.get('rdfLeaves', [])) > 0:
+                    # condition where there is no documentation
+                    embs = [term_embeddings['embs'][term_embeddings['id'].index(url_to_curie(o))] for o in rdf_leaves if url_to_curie(o) in term_embeddings['id']]
+                    if len(embs) > 0:
+                        cellml_emb = torch.mean(torch.stack(embs), dim=0)
+                    else: # no relevant information to be use, ignore
+                        continue
+                else:
+                    # no rdfleaves and documentation, ignore it
+                    continue
             else:
+                # condition where entity from documentation is available
                 cellml_emb = torch.mean(torch.stack([ent_embeddings[all_ents.index(cellml_ent)] for cellml_ent in cellml_ents]), dim=0)
             # check not_confirm term_id
-            for doc in cellml['documentation']:
+            for doc in cellml_id.get('documentation',[]):
                 for term_id in set(doc_termids[doc]['not_confirm']):
                     if util.cos_sim(tmp_term_embeddings[term_id], cellml_emb) >= 0.9:
                         pmr_terms[term_id] = not_confirm_terms[term_id]
                         term_embeddings['id'] += [term_id]
                         term_embeddings['embs'] += [tmp_term_embeddings[term_id]]
-                        if cellml_id not in cellml_to_terms:
-                            cellml_to_terms[cellml_id] = []
-                        cellml_to_terms[cellml_id] += [term_id]
+                        if cellml_url not in cellml_to_terms:
+                            cellml_to_terms[cellml_url] = []
+                        cellml_to_terms[cellml_url] += [term_id]
                 for term_id in set(doc_termids[doc]['confirm']):
                     pmr_terms[term_id] = self.__sckan_terms[term_id]
                     term_embeddings['id'] += [term_id]
                     idx = self.__sckan_biobert_embs['id'].index(term_id)
                     term_emb = self.__sckan_biobert_embs['embs'][idx]
                     term_embeddings['embs'] += [term_emb]
-                    if cellml_id not in cellml_to_terms:
-                        cellml_to_terms[cellml_id] = []
-                    cellml_to_terms[cellml_id] += [term_id]
+                    if cellml_url not in cellml_to_terms:
+                        cellml_to_terms[cellml_url] = []
+                    cellml_to_terms[cellml_url] += [term_id]
 
-            cellml_embeddings['id'] += [cellml_id]
+            cellml_embeddings['id'] += [cellml_url]
             cellml_embeddings['embs'] += [cellml_emb]
 
         # modify cellml_to_term as term_to_cellml
         term_to_cellml = {}
-        for cellml_id, cellml_terms in cellml_to_terms.items():
+        for cellml_url, cellml_terms in cellml_to_terms.items():
             for term_id in cellml_terms:
                 if term_id not in term_to_cellml:
                     term_to_cellml[term_id] = []
-                term_to_cellml[term_id] += [cellml_id]
+                term_to_cellml[term_id] += [cellml_url]
 
         # dump workspaces
         dumpJson(workspaces, RS_WORKSPACE)
 
+        # consolidate with the available search_data if not clean extraction
+        if not clean_extraction and search_data.get('term') is not None:
+            for i, term_id in enumerate(term_embeddings['id']):
+                if term_id not in search_data['term']:
+                    search_data['term'] += [term_id]
+                    search_data['embedding'] = torch.cat((search_data['embedding'], torch.stack([term_embeddings['embs'][i]])), 0)
+            for term_id, val in pmr_terms.items():
+                if term_id not in search_data['pmrTerm']:
+                    search_data['pmrTerm'][term_id] = val
+            for term_id, cellml_id in term_to_cellml.items():
+                if term_id not in search_data['termCellml']:
+                    search_data['termCellml'][term_id] = [cellml_id]
+                elif cellml_id not in search_data['termCellml'][term_id]:
+                    search_data['termCellml'][term_id] += [cellml_id]
+            for i, cellml_id in enumerate(cellml_embeddings['id']):
+                if cellml_id not in search_data['cellmlId']:
+                    search_data['cellmlId'] += [cellml_id]
+                    search_data['cellmlEmbs'] = torch.cat((search_data['cellmlEmbs'], torch.stack([cellml_embeddings['embs'][i]])), 0)
+
+            return {'id':search_data['term'], 'embs':search_data['embedding']}, search_data['pmrTerm'], cellmls, search_data['termCellml'], {'id':search_data['cellmlId'], 'embs':search_data['cellmlEmbs']}
         # return results
-        return cellmls, pmr_terms, term_to_cellml, term_embeddings, cellml_embeddings
+        term_embeddings['embs'] = torch.stack(term_embeddings['embs'])
+        cellml_embeddings['embs'] = torch.stack(cellml_embeddings['embs'])
+        return term_embeddings, pmr_terms, cellmls, term_to_cellml, cellml_embeddings
 
     def create_search_index(self, clean_extraction=True):
         self.__sckan_terms, self.__sckan_bert_embs, self.__sckan_biobert_embs = extract_sckan_terms(
@@ -362,20 +401,20 @@ class PMRIndexer:
         
         log.info(f'SCKAN has {self.__sckan_bert_embs["embs"].shape} terms of {BERTModel} and {self.__sckan_biobert_embs["embs"].shape} terms of {BIOBERT}')
 
-        cellmls, pmr_terms, term_to_cellml , term_embeddings, cellml_embeddings = self.__extract_pmr(clean_extraction)
+        term_embeddings, pmr_terms, cellmls, term_to_cellml, cellml_embeddings = self.__extract_pmr(clean_extraction)
 
         pmr_clusterer = CellmlClusterer(workspace_dir=self.__pmr_workspace_dir, cellmls=cellmls)
 
         combined_data = {
             'term':term_embeddings['id'],                       # a list of term having embedding
-            'embedding':torch.stack(term_embeddings['embs']),   # an array of term embedding
+            'embedding':term_embeddings['embs'],   # an array of term embedding
             'pmrTerm':pmr_terms,                                # a dictionary of terms in the PMR
             'sckanTerm':self.__sckan_terms,                     # a dictionary of terms in SCKAN
             'cellml':cellmls,                                   # a dictionary of CellML files
             'cluster':pmr_clusterer.getDict(),                  # a dictionary of CellML cluster
             'termCellml':term_to_cellml,                        # a dictionay terms in CellML files
             'cellmlId': cellml_embeddings['id'],                # a list of CellML files having embedding
-            'cellmlEmbs':torch.stack(cellml_embeddings['embs']) # an array of CellML embedding
+            'cellmlEmbs':cellml_embeddings['embs'] # an array of CellML embedding
             }
 
         ### safe to file
